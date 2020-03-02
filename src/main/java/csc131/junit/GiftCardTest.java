6dd47bc44e47a8045d0b7a8a460422ec063c3542
lospe <lospe@10.0.0.213>
2020-03-01 18:31:15 -0800

    Initial Creation of TestClass

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,14 @@
+package csc131.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GiftCardTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented");
+	}
+
+}
