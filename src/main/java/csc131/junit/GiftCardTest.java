230609b7d11ea4de0d78cbd5d3a3d9e4faf44777
lospe <lospe@10.0.0.213>
2020-03-01 18:39:15 -0800

    Added code

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,14 +1,27 @@
 package csc131.junit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GiftCardTest {
-
+	double balance;
+	GiftCard card;
+	int issuingStore;
+	
+	@BeforeEach
+	void setUp() {
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+	}
+	
 	@Test
-	final void test() {
-		fail("Not yet implemented");
+	void testGetIssuingStore() {
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
 	}
 
 }
