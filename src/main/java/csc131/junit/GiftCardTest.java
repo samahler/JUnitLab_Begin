74824a8001a1ecbc3cc956ad9f93f794b92f1c65
Samantha <babygirl@Samanthas-MacBook-Pro.local>
2020-03-05 12:30:55 -0800

    Finished Part 2 of Junit

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,27 +1,92 @@
 package csc131.junit;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class GiftCardTest {
-	double balance;
-	GiftCard card;
-	int issuingStore;
-	
-	@BeforeEach
-	void setUp() {
-		issuingStore = 1337;
-		balance = 100.00;
-		card = new GiftCard(issuingStore, balance);
-	}
-	
+public class GiftCardTest
+{
 	@Test
-	void testGetIssuingStore() {
+	public void getIssuingStore()
+	{
+		double       balance;        
+		GiftCard     card;
+		int          issuingStore;
+		
+		issuingStore = 1337;
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
 		
 		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
 	}
-
+	
+	@Test
+	public void getBalance()
+	{
+		double       balance;        
+		GiftCard     card;
+		int          issuingStore;
+		
+		issuingStore = 1337;
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test
+	public void deduct()
+	{
+		double       balance;        
+		GiftCard     card;
+		int          issuingStore;
+		String       remainingBalance;
+		
+		
+		issuingStore     = 1337;
+		balance          = 100.00;
+		remainingBalance = "Remaining Balance: " + String.format("%6.2f", 60.00);
+		
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("deduct(40.00)", remainingBalance, card.deduct(40.00));
+	}
+	
+	@Test
+	public void deduct_negativeDeduction()
+	{
+		double       balance;        
+		GiftCard     card;
+		int          issuingStore;
+		String       remainingBalance;
+		
+		
+		issuingStore     = 1337;
+		balance          = 100.00;
+		remainingBalance = "Invalid Transaction";
+		
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("deduct(-40.00)", remainingBalance, card.deduct(-40.00));
+	}
+	
+	@Test
+	public void deduct_negativeBalance()
+	{
+		double       balance;        
+		GiftCard     card;
+		int          issuingStore;
+		String       remainingBalance;
+		
+		
+		issuingStore     = 1337;
+		balance          = 100.00;
+		remainingBalance = "Amount Due: " + String.format("%6.2f", 1.00);
+		
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("deduct(101.00)", remainingBalance, card.deduct(101.00));
+	}
+	
+	
 }
